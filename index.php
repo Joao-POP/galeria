60054f74631ff79b2c7923e27e360e6ba85efe4e
Joao-POP <joaopap@proton.me>
2026-09-08 16:10:41 -0300

    Added upload form

--- index.php
+++ index.php
@@ -7,9 +7,19 @@
 		<script src="main.js" type="module" defer></script>
 	</head>
 	<body>
-		<button class="button-background">
+		<button id="upload-button" class="button-background">
 			<div class="vertical-arm"></div>
 			<div class="horizontal-arm"></div>
 		</button>
+		<div id="upload-window">
+			<label>Enviar arquivos</label>
+			<button class="close-button"><div class="cross"></div></button>
+			<form method="post" action="" enctype="multipart/form-data">
+				<input type="file" accept="image/*" name="image-upload" />
+				<button>Enviar</button>
+				<button>Escolher outra foto</button>
+				<button>Cancelar</button>
+			</form>
+		</div>
 	</body>
 </html>
